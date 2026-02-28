be53f98d1e25c4e38bc6a0f49773d45897bfb1ae
Liam Wahlberg <liam.wahlberg07@gmail.com>
2026-02-28 20:42:23 +0100

    cleanup

--- src/main/java/me/liqw/locatorborder/mixin/GuiMixin.java
+++ src/main/java/me/liqw/locatorborder/mixin/GuiMixin.java
@@ -4,14 +4,7 @@ import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.contextualbar.ContextualBarRenderer;
 import net.minecraft.client.gui.contextualbar.LocatorBarRenderer;
-import net.minecraft.client.renderer.RenderPipelines;
-import net.minecraft.client.resources.WaypointStyle;
-import net.minecraft.resources.Identifier;
-import net.minecraft.util.ARGB;
-import net.minecraft.util.Mth;
-import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,8 +12,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.function.Supplier;
 
 @Mixin(Gui.class)
 public abstract class GuiMixin {
